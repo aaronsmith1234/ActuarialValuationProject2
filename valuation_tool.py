dacc58e6acd4f6f57b1a7c288743d9ea910c142c
Aaron Smith <aaronsmith1234@gmail.com>
2021-03-23 21:19:25 -0700

    moving the data to its own file

--- valuation_tool.py
+++ valuation_tool.py
@@ -1,21 +1,6 @@
-import pandas as pd
+import import_data
 
-# read in Data
-ageFactors = pd.read_csv("ageFactors.csv")
-ageFactors["ageFactor"] = ageFactors["ageFactor"].str.rstrip("%").astype(float) / 100
-
-genderFactors = pd.read_csv("genderFactors.csv")
-genderFactors["genderFactor"] = (
-    genderFactors["genderFactor"].str.rstrip("%").astype(float) / 100
-)
-
-inforceFile = pd.read_csv("inforceFile.csv")
-inforceFile["AV"] = inforceFile["AV"].str.lstrip(" $").astype(float)
-
-# merge inforce file and assumptions
-inforceFile = inforceFile.merge(genderFactors)
-inforceFile = inforceFile.sort_values("Age")
-inforceFile = pd.merge_asof(inforceFile, ageFactors)
+inforceFile = import_data.read_in_data()
 
 # calculate reserve
 inforceFile["Reserve"] = (
