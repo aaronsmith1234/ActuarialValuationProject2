accc604c3bf487f04add9af8c3bbfad8eb3fcab6
Aaron Smith <aaronsmith1234@gmail.com>
2021-03-23 21:39:20 -0700

    adding my changes

--- import_data.py
+++ import_data.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
+input_folder = "input_files/"
+
 
 def read_in_data():  # read in Data
-    ageFactors = pd.read_csv("ageFactors.csv")
+    ageFactors = pd.read_csv(input_folder + "ageFactors.csv")
     ageFactors["ageFactor"] = (
         ageFactors["ageFactor"].str.rstrip("%").astype(float) / 100
     )
 
-    genderFactors = pd.read_csv("genderFactors.csv")
+    genderFactors = pd.read_csv(input_folder + "genderFactors.csv")
     genderFactors["genderFactor"] = (
         genderFactors["genderFactor"].str.rstrip("%").astype(float) / 100
     )
 
-    inforceFile = pd.read_csv("inforceFile.csv")
+    inforceFile = pd.read_csv(input_folder + "inforceFile.csv")
     inforceFile["AV"] = inforceFile["AV"].str.lstrip(" $").astype(float)
 
     # merge inforce file and assumptions
